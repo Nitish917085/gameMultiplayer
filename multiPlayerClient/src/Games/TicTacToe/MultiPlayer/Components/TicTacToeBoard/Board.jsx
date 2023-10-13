53904f383d44d23e6c764fed1a5ddba78f7227cc
Nitish917085 <nitish917085@gmail.com>
2023-10-13 09:46:08 +0530

    simplified tic tac toe logics

--- multiPlayerClient/src/Games/TicTacToe/MultiPlayer/Components/TicTacToeBoard/Board.jsx
+++ multiPlayerClient/src/Games/TicTacToe/MultiPlayer/Components/TicTacToeBoard/Board.jsx
@@ -1,122 +1,112 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../TicTacToe.css'
 import { useSelector } from 'react-redux';
 
 const calculateWinner = (squares) => {
-	const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
-
 
 const Board = ({ socket, room_id }) => {
+  const user = useSelector(state => state.user);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [chance, setChance] = useState(1);
+  const [player, setPlayer] = useState('');
+  
+  const winner = calculateWinner(squares);
+  let status;
 
-	const user = useSelector(state => state.user)
+  if (winner) {
+    status = (player === user.userName) ? 'You won' : 'Better Luck Next Time';
+  } else {
+    status = (player !== user.userName) ? 'Your Chance' : 'Opponent Chance';
+  }
 
-	const [squares, setSquares] = useState(Array(9).fill(null))
-	const [, updateState] = React.useState();
-	const forceUpdate = React.useCallback(() => updateState({}), []);
-	const xIsNext = useRef(true);
-	const Chance = useRef(1);
-	const Player = useRef('');
+  useEffect(() => {
+    socket.on('squareClickedReceived', click => {
+      const i = click.i;
+      const updatedSquares = [...squares]; // Create a new array to avoid mutating state directly
+      if (!calculateWinner(updatedSquares) && !updatedSquares[i]) {
+        updatedSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(updatedSquares);
+        setXIsNext(!xIsNext);
+        setPlayer(click.user_id);
+        if (chance === 2) {
+          setChance(1);
+        } else if (chance === -1) {
+          setChance(2);
+        }
+      }
+    });
+  }, [squares, xIsNext, chance]);
 
-	const winner = calculateWinner(squares);
-	let status;
-	if (winner) {
-		status = (Player.current === user.userName) ? 'You winned ' : 'Better Luck Next Time';
-		// status = 'Better Luck Next Time:' +Player.current;
-	} else {
-		status = (Player.current !== user.userName) ? 'Your Chance' : 'Opponent Chance';
-	}
+  useEffect(() => {
+    socket.on('playAgainReceived', () => {
+      setSquares(Array(9).fill(null));
+      setChance(1);
+      setPlayer('');
+    });
+  }, []);
 
-	useEffect(() => {
-		socket.on('squareClickedReceived', click => {
-			const i = click.i;
-			squares[i] = xIsNext.current ? 'X' : 'O';
-			xIsNext.current = !xIsNext.current;
-			setSquares(squares);
+  const handleClick = (i) => {
+    if (chance === 2 || chance === -1 || calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const click = {
+      i,
+      name: user.nickName,
+      user_id: user.userName,
+      room_id
+    };
+    socket.emit('squareClicked', click);
+    setChance(-1);
+  }
 
-			Player.current = click.user_id;
+  const PlayAgain = () => {
+    socket.emit('playAgain', room_id);
+  }
 
-			if (Chance.current === 2) Chance.current = 1;
-			if (Chance.current === -1) Chance.current = 2;
-			console.log(squares);
-			forceUpdate();
-		})
-	}, [squares, xIsNext])
+  const renderSquare = (i) => {
+    return (
+      <div onClick={() => handleClick(i)}>
+        {squares[i] ? squares[i] : ''}
+      </div>
+    );
+  }
 
-
-	useEffect(() => {
-		socket.on('playAgainReceived', () => {
-			squares.fill(null);
-			setSquares(squares)
-			console.log(squares);
-			Chance.current = 1;
-			Player.current = '';
-			forceUpdate();
-		})
-	}, [squares])
-
-
-	const handleClick = (i) => {
-
-		if (Chance.current === 2 || Chance.current === -1 || calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		// console.log('emitting');
-		const click = {
-			i,
-			name: user.nickName,
-			user_id: user.userName,
-			room_id
-		};
-		socket.emit('squareClicked', click);
-		Chance.current = -1;
-	}
-
-	const PlayAgain = () => {
-		socket.emit('playAgain', room_id);
-	}
-
-	const renderSquare = (i) => {
-		return (
-			<div onClick={() => handleClick(i)} >
-				{squares[i] ? squares[i] : ''}
-			</div>
-		)
-	}
-
-	return (
-		<div className="boardContainer">
-			<div className='boardSlate'>
-				<div className="status">{status}</div>
-				<div className="Board_game">
-					<div className="board-row">
-						{renderSquare(0)}
-						{renderSquare(1)}
-						{renderSquare(2)}
-					</div>
-					<div className="board-row">
-						{renderSquare(3)}
-						{renderSquare(4)}
-						{renderSquare(5)}
-					</div>
-					<div className="board-row">
-						{renderSquare(6)}
-						{renderSquare(7)}
-						{renderSquare(8)}
-					</div>
-				</div>
-				<br/>
-				<div className='playAgainButton' ><div onClick={PlayAgain}>Play Again</div></div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="boardContainer">
+      <div className='boardSlate'>
+        <div className="status">{status}</div>
+        <div className="Board_game">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <br/>
+        <div className='playAgainButton'><div onClick={PlayAgain}>Play Again</div></div>
+      </div>
+    </div>
+  );
 }
 
-export default Board
+export default Board;
